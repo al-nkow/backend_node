577fa2fc469a875e6f11f1987e40db5036f2d3b0
Aleksey Kovalchuk <al.nkow@gmail.com>
2018-05-15 23:56:17 +0300

    init

---
+++ app.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const path  = require('path'); // <<<<<-----
+
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
+
+mongoose.connect('mongodb://localhost:27017/myapi', {
+    // useMongoClient: true
+});
+
+// mongoose.Promise = global.Promise; // fix some error
+
+app.use(morgan('dev'));
+
+// publicly available routes
+//app.use(express.static('uploads')); // url: http://localhost:3000/filename.jpg !!! no /uploads/!!!!
+
+app.use('/uploads', express.static('uploads'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    // Allow CORS
+    res.header('Access-Control-Allow-Origin', '*'); // * - allow from any url
+    // res.header('Access-Control-Allow-Origin', 'http://my-cool-page.com')
+    // res.header('Access-Control-Allow-Header', '*')
+    res.header('Access-Control-Allow-Header', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+// Routes
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
+
+
+
+
+// INDEX.html =========
+app.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
+});
+app.get('/admin',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/admin/admin.html'));
+});
+
+
+
+
+
+
+// error handler middleware
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// error handler middleware
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// });
+
+module.exports = app;
